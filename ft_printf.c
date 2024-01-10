b8f029192f80623433db82f62ad5c6a3df454c55
Molasz <martiolasz@gmail.com>
2024-01-10 01:19:59 +0100

    args setup

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,37 @@
 /*   By: molasz-a <molasz-a@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 23:13:29 by molasz-a          #+#    #+#             */
-/*   Updated: 2024/01/09 23:57:16 by molasz-a         ###   ########.fr       */
+/*   Updated: 2024/01/10 01:19:08 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	check_args(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (!ft_strrchr("cspduxX%", s[i]))
+	{
+		if (s[i] == '-')
+			ft_putchar_fd('-', 1);
+		else if (s[i] == '0')
+			ft_putchar_fd('0', 1);
+		else if (s[i] == '.')
+			ft_putchar_fd('.', 1);
+		else if (s[i] == '#')
+			ft_putchar_fd('#', 1);
+		else if (s[i] == ' ')
+			ft_putchar_fd(' ', 1);
+		else if (s[i] == '+')
+			ft_putchar_fd('+', 1);
+		else
+			ft_putstr_fd("ERROR", 1);
+		i++;
+	}
+	return (i);
+}
 
 int	ft_printf(const char *s, ...)
 {
@@ -22,11 +48,7 @@ int	ft_printf(const char *s, ...)
 	while (s[i])
 	{
 		if (s[i] == '%')
-		{
-			i++;
-			ft_putchar_fd(s[i], 1);
-
-		}
+			check_args(&((char *)s)[i]);
 		else
 			ft_putchar_fd(s[i], 1);
 		i++;
